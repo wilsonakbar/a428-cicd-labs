a7b09046106ccaf66ac563857c0d2d5e8c529dab
WE <we@MacBook-Pro-MBPro.local>
2025-07-24 23:34:49 +0700

    update

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -5,6 +5,9 @@ echo 'production in the local "build" directory (i.e. within the'
 echo '"/var/jenkins_home/workspace/simple-node-js-react-app" directory),'
 echo 'correctly bundles React in production mode and optimizes the build for'
 echo 'the best performance.'
+
+export NODE_OPTIONS=--openssl-legacy-provider
+
 set -x
 npm run build
 set +x
@@ -17,6 +20,9 @@ echo 'can pause running builds of CI/CD applications indefinitely. "npm start"'
 echo 'is followed by another command that retrieves the process ID (PID) value'
 echo 'of the previously run process (i.e. "npm start") and writes this value to'
 echo 'the file ".pidfile".'
+
+export HOST=0.0.0.0
+
 set -x
 npm start &
 sleep 1
